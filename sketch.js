fac85d661768dc903d5f6ea922f8294f1dde2403
zerothehero555 <73942835+zerothehero555@users.noreply.github.com>
2021-01-27 18:58:01 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,28 @@
+var canvas;
+var gameState = 0;
+var contestantCount;
+var database;
+var quiz;
+var question;
+var contestant;
+
+
+
+function setup(){
+  canvas = createCanvas(850,400);
+  database = firebase.database()
+  quiz = new Quiz()
+  quiz.getState()
+  quiz.start()
+}
+
+
+function draw(){
+  background("pink");
+ if(contestantCount === 4){ quiz.update(1); } 
+ if(gameState === 1){ 
+   clear(); 
+   quiz.play(); 
+  }
+
+}
